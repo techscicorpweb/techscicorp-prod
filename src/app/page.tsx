7c64b58c2488b401ea0ceea695573b91656214d1
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-13 00:19:53 -0400

    TSC logo file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
+          src="/tsc-logo-only-transparent.png"
           alt="Next.js logo"
           width={100}
           height={20}
