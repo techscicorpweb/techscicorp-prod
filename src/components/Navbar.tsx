ce2da10e5f1896f09144f77ad0280e9743580759
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-13 00:26:13 -0400

    TSC logo in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,32 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="bg-[#0B2545] text-white">
-      <div className="mx-auto max-w-7xl px-6 py-4 flex gap-6">
-        
-		<Link href="/.">Home
-		</Link>
-		<Link href="./capabilities">Capabilities
-		</Link>
-		<Link href="./about">About TSC
-		</Link>
-		<Link href="./careers">Careers
-		</Link>
-		<Link href="./contact">Contact Us
-		</Link>
+      <div className="mx-auto max-w-7xl px-6 py-4 flex items-center justify-between gap-6">
+
+        <Image
+          src="/tsc-logo-only-transparent.png"
+          alt="TSC Logo"
+          width={120}
+          height={80}
+          className="h-10 w-auto"
+        />
+
+        <div className="flex gap-6">
+          <Link href="/.">Home
+          </Link>
+          <Link href="./capabilities">Capabilities
+          </Link>
+          <Link href="./about">About TSC
+          </Link>
+          <Link href="./careers">Careers
+          </Link>
+          <Link href="./contact">Contact Us
+          </Link>
+        </div>
+
       </div>		
 	</nav>
   );
