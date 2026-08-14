096b65effe1998c81e1e651273a8c3c57d32d9be
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-13 21:33:42 -0400

    about page updates corporate profile

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,26 +1,27 @@
 export default function AboutPage() {
   return (
-    {/*<main className="max-w-7xl mx-auto px-6 py-16 bg-slate-900 text-white">*/},
-      {/* Hero Section */},
+    <main>
+      {/* Hero Section */}
       <section className="bg-slate-900 text-white py-24">
         <div className="container mx-auto px-6 max-w-7xl">
 	      <h1 className="text-4xl font-bold mb-6">
            About Technology Science Corporation
           </h1>
-      <p className="text-lg">
-        Technology Science Corporation supports
-        government agencies through innovative
-        technology solutions and mission-focused
-        service delivery.
-      </p>
-
-      {/* Added top margin (mt-10) for spacing above */},
-      <h2 className="text-2xl font-semibold mt-10 mb-4">
-        Leadership
-      </h2>
-	  <h2 className="text-2xl font-semibold mt-10 mb-4">
-        Corporate Profile
-	  </h2>
-    </main>
+          <p className="text-lg">
+           Technology Science Corporation supports
+           government agencies through innovative
+           technology solutions and mission-focused
+           service delivery.
+          </p>
+      {/* Added top margin (mt-10) for spacing above */}
+          <h2 className="text-2xl font-semibold mt-10 mb-4">
+           Leadership
+          </h2>
+	      <h2 className="text-2xl font-semibold mt-10 mb-4">
+           Corporate Profile
+	      </h2>
+	    </div>
+      </section>
+	</main>
   );
 }
