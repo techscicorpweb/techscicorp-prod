c3d71b6b300c1eb7fed3c260da29d2ee1f947813
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-14 03:04:07 -0400

    Spacing in paragraph

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -105,9 +105,12 @@ export default async function AboutPage() {
             Technology Science Corporation supports government agencies
             through innovative technology solutions and mission-focused
             service delivery.
-            
-            We encourage and promote a culture heavily vested in creative problem solving, responsiveness, process improvement, high quality results, and out-of-the box best value solutions. We execute by listening to an agency’s issues, encourage transparent communications to meet tight deadlines, and perform with integrity.
+            <p>
+            We encourage and promote a culture heavily vested in creative problem solving, responsiveness, process improvement, high quality results, and out-of-the box best value solutions. We execute by listening to an agency’s issues, encourage transparent communications to meet tightdeadlines, and perform with integrity.
+            </p>
+            <p>
             We understand the federal procurement framework and the importance of collaborating with Enduser, Program Management, and Contracting to execute within budget. Our goal is to be your agency’s trusted IT partner of choice!
+            </p>
           </p>
 
           {/* Added top margin (mt-10) for spacing above */}
