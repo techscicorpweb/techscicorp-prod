9dbb5bd504ef617bacacf2bcc985413c3bcde9b0
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-17 01:20:02 -0400

    Home banner image for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
       <section className="relative py-24 overflow-hidden bg-slate-100">
         {/* Responsive background image */}
         <Image
-          src="/images/home-banner.jpg"
+          src="/images/home-banner.png"
           alt=""
           fill
           priority
