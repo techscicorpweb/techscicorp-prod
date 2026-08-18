b4759be16f2c312fab1581f9ae3b6794c50a858e
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-18 00:59:45 -0400

    Analytics code in layout

--- src/app/layout - Copy.tsx
+++ src/app/layout - Copy.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -9,6 +8,7 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -31,7 +31,6 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           {children}
         </main>
         <Footer />
-        <Analytics />
 	  </body>
     </html>
   );
