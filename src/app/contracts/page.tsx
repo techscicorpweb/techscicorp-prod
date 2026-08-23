2750bc1f9ec5cba1e148ec84e65c587824ca335f
Brian Rupert <brian.rupert@techscicorp.com>
2026-08-23 11:10:22 -0400

    Update MAS ITG2 link

--- src/app/contracts/page.tsx
+++ src/app/contracts/page.tsx
@@ -27,7 +27,7 @@ const contracts = [
   
    {
     title: "GSA MAS: 47QTCA25D007G (TSC-ITG2 JV, LLC)",
-    linkHref: "https://www.gsaelibrary.gsa.gov/ElibMain/contractorInfo.do?contractNumber=GS-35F-445GA&contractorName=TECHNOLOGY+SCIENCE+CORP",
+    linkHref: "https://www.gsaelibrary.gsa.gov/ElibMain/contractorInfo.do?contractNumber=GS-35F-445GA&contractorName=TECHNOLOGY+SCIENCE+CORP&executeQuery=YES",
     period: "Contract Period through Mar 26, 2030",
     details: [
       "SIN:  54151S Information Technology Professional Services",
